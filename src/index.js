5c7b5abe9079af6680df6ead6d3012f7872736b9
A.Markelychev <a.markelychev@gmail.com>
2020-10-07 00:40:43 +0300

    feat: add toReadable module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 module.exports = function toReadable(number) {
-    const numAsArr = number.toString().split("");
-    const readable = [
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten",
-        "eleven",
-        "twelve",
-        "thirteen",
-        "fourteen",
-        "fifteen",
-        "sixteen",
-        "seventeen",
-        "eighteen",
-        "nineteen",
-        "twenty",
-    ];
+    let num = number;
+    const readable = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+    };
+    let res = "";
+    if (num >= 100) {
+        res += readable[Math.floor(num / 100)] + " hundred";
+        num -= Math.floor(num / 100) * 100;
+    }
+    if (num > 20) {
+        if (res) res += " ";
+        res += readable[Math.floor(num / 10) * 10];
+        num -= Math.floor(num / 10) * 10;
+    }
+
+    if (num > 0) {
+        if (res) res += " ";
+        res += readable[num];
+    }
+
+    if (!number) res += readable[num];
+    return res;
 };
